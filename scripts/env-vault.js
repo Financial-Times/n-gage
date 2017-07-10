48fd1fc42d00c06fc0a68399936ee8eeffab916b
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2017-07-10 14:54:22 +0100

    Continue to fetch .env vars from vault, even if the app has no shared secrets.  🐿2.5.9

--- scripts/env-vault.js
+++ scripts/env-vault.js
@@ -7,21 +7,25 @@ const app = process.argv[2].replace(/^ft-/, '');
 const token = fs.readFileSync(path.join(os.homedir(), '.vault-token'), { encoding: 'utf8' });
 
 const vault = path => fetch('https://vault.in.ft.com/v1/' + path, { headers: { 'X-Vault-Token': token } })
-    .then(json => json.data || {});
+    .then(json => json.data || {})
+    .catch(error => {
+        console.error(`Error fetching ${path}`);
+        return true;
+    })
 
 Promise.all([
     vault(`secret/teams/next/${app}/development`),
     vault(`secret/teams/next/${app}/shared`),
     vault('secret/teams/next/shared/development'),
 ])
-    .then(([app, appShared, envShared]) => {        
-        const shared = appShared.env.reduce((keys, key) => {
+    .then(([app, appShared, envShared]) => {
+        const shared = appShared.env && appShared.env.reduce((keys, key) => {
             if (key in envShared) {
                 keys[key] = envShared[key];
             }
-            
+
             return keys;
-        }, {});
+        }, {}) || {};
 
         const keys = Object.assign({}, shared, app);
 
