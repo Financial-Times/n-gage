d5aaa79a8b3ef88ec85823c2ea9b2fdfc9592a56
Ben Fletcher <ben.fletcher@ft.com>
2017-08-02 11:48:01 +0100

    simplified option

--- scripts/get-config.js
+++ scripts/get-config.js
@@ -1,15 +1,56 @@
+#!/usr/bin/env node
+
 const opts = require('yargs').argv;
 const fetch = require('@financial-times/n-fetch');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
+
+const getToken = () => {
+	if (process.env.CIRCLECI) {
+		// on CircleCI
+		return fetch('https://vault.in.ft.com/v1/auth/approle/login', {
+			method: 'POST',
+			body: JSON.stringify({ role_id: process.env.VAULT_ROLE_ID, secret_id: process.env.VAULT_SECRET_ID })
+		})
+		.then(json => json.auth.client_token)
+	} else {
+		// developer's local machine
+		return new Promise((resolve, reject) => {
+			return fs.readFile(path.join(os.homedir(), '.vault-token'), { encoding: 'utf8' }, (error, data) => {
+				if (error) {
+					reject(error);
+				} else {
+					resolve(data);
+				}
+			});
+		});
+	}
+};
 
 const getVaultPaths = (ftApp, env) => {
 	const app = ftApp.replace(/^ft-/, '');
+	const vaultEnvs = { dev: 'development', prod: 'production', ci: 'continuous-integration' };
+	const vaultEnv = vaultEnvs[env];
 	return [
-		`secret/teams/next/${app}/${env}`,
+		`secret/teams/next/${app}/${vaultEnv}`,
 		`secret/teams/next/${app}/shared`,
-		`secret/teams/next/shared/${env}`
+		`secret/teams/next/shared/${vaultEnv}`
 	];
+};
+
+const parseKeys = (app, appShared, envShared) => {
+	if (opts.env === 'ci') {
+		return Object.assign({}, app.env, envShared);
+	} else {
+		const shared = appShared.env.reduce((keys, key) => {
+			if (key in envShared) {
+				keys[key] = envShared[key];
+			}
+			return keys;
+		}, {});
+		return Object.assign({}, shared, app);
+	}
 };
 
 const format = (keys, mode) => {
@@ -22,33 +63,25 @@ const format = (keys, mode) => {
 	}
 };
 
+// LET'S GO!
+
 if (!opts.app || !opts.env) {
-	console.error('\nusage:\n\nget-config --app next-app --env (development|production|continuous-integration) --format (simple|json) --filename .env\n');
+	console.error('\nusage:\n\nget-config [options]\n\n--app next-app-name [required]\n--env (dev|prod|ci) [required]\n--format (simple|json) [optional - defaults to simple]\n--filename .env [optional - defaults to .env]\n');
 	process.exit(14);
 }
 
-fetch('https://vault.in.ft.com/v1/auth/approle/login', {
-    method: 'POST',
-    body: JSON.stringify({ role_id: process.env.VAULT_ROLE_ID, secret_id: process.env.VAULT_SECRET_ID })
-})
-	.then(json => json.auth.client_token)
+getToken()
 	.then(token => {
 		return Promise.all(getVaultPaths(opts.app, opts.env).map(path => {
 			return fetch('https://vault.in.ft.com/v1/' + path, { headers: { 'X-Vault-Token': token } })
 				.then(json => json.data || {})
     }))
 			.then(([app, appShared, envShared]) => {        
-				const shared = appShared.env.reduce((keys, key) => {
-					if (key in envShared) {
-						keys[key] = envShared[key];
-					}
-					return keys;
-				}, {});
-        const keys = Object.assign({}, shared, app);
+				const keys = parseKeys(app, appShared, envShared)
 				const content = format(keys, opts.format);
 				const file = path.join(process.cwd(), opts.filename || '.env');
 				fs.writeFileSync(file, content);
-				console.log(`Written to .env. File: ${file}`);
+				console.log(`Written ${opts.app}'s ${opts.env} config to ${file}`);
     });
 	})
 		.catch(error => {
--- test/get-config.test.js
+++ test/get-config.test.js
@@ -6,15 +6,20 @@ const requireUncached = require('require-uncached');
 
 describe('get-config', () => {
 
+	after(() => {
+		delete process.env.CIRCLECI;
+	});
+
 	it('writes in simple format', (done) => {
 		const fetch = sinon.stub();
 		const writeFileSync = sinon.spy();
+		process.env.CIRCLECI = 1;
 		fetch.withArgs('https://vault.in.ft.com/v1/auth/approle/login', sinon.match.object).returns(Promise.resolve({ auth: { client_token: 'mytoken' }}));
 		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/production', sinon.match.object).returns(Promise.resolve({ data: { a: 'z' }}));
 		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/shared', sinon.match.object).returns(Promise.resolve({ data: { env: [ 'b' ]}}));
 		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/shared/production', sinon.match.object).returns(Promise.resolve({ data: { b: 'y' }}));
 		const script = proxyquire('../scripts/get-config', {
-			'yargs': { argv: require('yargs')(['', '', '--app', 'myapp', '--env', 'production', '--format', 'simple']).argv },
+			'yargs': { argv: require('yargs')(['', '', '--app', 'myapp', '--env', 'prod', '--format', 'simple']).argv },
 			'@financial-times/n-fetch': fetch,
 			'fs': { writeFileSync }
 		});
@@ -28,18 +33,66 @@ describe('get-config', () => {
 	it('writes in JSON format', (done) => {
 		const fetch = sinon.stub();
 		const writeFileSync = sinon.spy();
+		process.env.CIRCLECI = 1;
 		fetch.withArgs('https://vault.in.ft.com/v1/auth/approle/login', sinon.match.object).returns(Promise.resolve({ auth: { client_token: 'mytoken' }}));
-		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/production', sinon.match.object).returns(Promise.resolve({ data: { a: 'z' }}));
-		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/shared', sinon.match.object).returns(Promise.resolve({ data: { env: [ 'b' ]}}));
-		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/shared/production', sinon.match.object).returns(Promise.resolve({ data: { b: 'y' }}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/development', { headers: { 'X-Vault-Token': 'mytoken' }}).returns(Promise.resolve({ data: { a: 'z' }}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/shared', { headers: { 'X-Vault-Token': 'mytoken' }}).returns(Promise.resolve({ data: { env: [ 'b' ]}}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/shared/development', { headers: { 'X-Vault-Token': 'mytoken' }}).returns(Promise.resolve({ data: { b: 'y' }}));
 		const script = proxyquire('../scripts/get-config', {
-			'yargs': { argv: require('yargs')(['', '', '--app', 'myapp', '--env', 'production', '--format', 'json']).argv },
+			'yargs': { argv: require('yargs')(['', '', '--app', 'myapp', '--env', 'dev', '--format', 'json']).argv },
 			'@financial-times/n-fetch': fetch,
 			'fs': { writeFileSync }
 		});
 		setTimeout(() => {
 			expect(writeFileSync).to.have.been.called;
 			expect(writeFileSync).to.have.been.calledWith(sinon.match.string, '{\n  "b": "y",\n  "a": "z"\n}');
+			done();
+		}, 0);
+	});
+
+	it('uses local token if not on CircleCI', (done) => {
+		const fetch = sinon.stub();
+		const homedir = sinon.stub();
+		const readFile = (path, opts, cb) => {
+			if (path === '/path/to/home/.vault-token') {
+				cb(null, 'my-token');
+			}
+		};
+		const writeFileSync = sinon.spy();
+		delete process.env.CIRCLECI;
+		homedir.returns('/path/to/home');
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/production', { headers: { 'X-Vault-Token': 'my-token' }}).returns(Promise.resolve({ data: { a: 'z' }}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/shared', { headers: { 'X-Vault-Token': 'my-token' }}).returns(Promise.resolve({ data: { env: [ 'b' ]}}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/shared/production', { headers: { 'X-Vault-Token': 'my-token' }}).returns(Promise.resolve({ data: { b: 'y' }}));
+		const script = proxyquire('../scripts/get-config', {
+			'yargs': { argv: require('yargs')(['', '', '--app', 'myapp', '--env', 'prod', '--format', 'simple']).argv },
+			'@financial-times/n-fetch': fetch,
+			'fs': { readFile, writeFileSync },
+			'os': { homedir }
+		});
+		setTimeout(() => {
+			expect(writeFileSync).to.have.been.called;
+			expect(writeFileSync).to.have.been.calledWith(sinon.match.string, 'a=z\nb=y\n');
+			done();
+		}, 0);
+	});
+
+	it('extracts env from payload if continuous integration', (done) => {
+		const fetch = sinon.stub();
+		const writeFileSync = sinon.spy();
+		process.env.CIRCLECI = 1;
+		fetch.withArgs('https://vault.in.ft.com/v1/auth/approle/login', sinon.match.object).returns(Promise.resolve({ auth: { client_token: 'mytoken' }}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/continuous-integration', sinon.match.object).returns(Promise.resolve({ data: { env: { a: 'z' }}}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/myapp/shared', sinon.match.object).returns(Promise.resolve({ data: {}}));
+		fetch.withArgs('https://vault.in.ft.com/v1/secret/teams/next/shared/continuous-integration', sinon.match.object).returns(Promise.resolve({ data: { b: 'y' }}));
+		const script = proxyquire('../scripts/get-config', {
+			'yargs': { argv: require('yargs')(['', '', '--app', 'myapp', '--env', 'ci', '--format', 'simple']).argv },
+			'@financial-times/n-fetch': fetch,
+			'fs': { writeFileSync }
+		});
+		setTimeout(() => {
+			expect(writeFileSync).to.have.been.called;
+			expect(writeFileSync).to.have.been.calledWith(sinon.match.string, 'a=z\nb=y\n');
 			done();
 		}, 0);
 	});
