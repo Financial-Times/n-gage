3a3d0a640bb863f8b9d9f533760ff6d3f928b2b2
Dora Militaru <dora.militaru@ft.com>
2017-10-11 12:11:38 +0100

    use npm version instead of prepublish (#78)
    * use npm version instead of prepublish

--- index.mk
+++ index.mk
@@ -251,7 +251,11 @@ endif
 # DEPLOY SUB-TASKS
 
 npm-publis%: ## npm-publish: Publish this package to npm.
+ifneq ($(CIRCLE_TAG),)
+	npm version --no-git-tag-version ${CIRCLE_TAG}
+else
 	npm-prepublish --verbose
+endif
 	npm publish --access public
 
 # BUILD SUB-TASKS
