1d9659c02af705ed03e76aa544ad09b55e276465
Tunca Bergmen <tbergmen@gmail.com>
2018-03-20 09:17:24 +0000

    Fix Node 6 compatibility issue  🐿 v2.7.0

--- scripts/append-session-tokens.js
+++ scripts/append-session-tokens.js
@@ -1,6 +1,6 @@
 const fetch = require('@financial-times/n-fetch');
 
-module.exports = async (keys) => {
+module.exports = (keys) => {
 	const { TEST_USER_TYPES, TEST_SESSIONS_URL, TEST_SESSIONS_API_KEY } = keys;
 	if (!(TEST_USER_TYPES && TEST_SESSIONS_API_KEY && TEST_SESSIONS_URL)) {
 		return keys;
@@ -15,17 +15,18 @@ module.exports = async (keys) => {
 		});
 };
 
-const appendToken = async (keys, userType, url, apiKey) => {
-	try {
-		const tokens = await fetch(`${url}/${userType}?api_key=${apiKey}`);
-		if (tokens.FTSession) {
-			keys[`${userType.toUpperCase()}_FTSession`] = tokens.FTSession;
-		}
-		if (tokens.FTSession_s) {
-			keys[`${userType.toUpperCase()}_FTSession_s`] = tokens.FTSession_s;
-		}
-	} catch (e) {
-		console.error(e);
-		console.log(`Couldn\'t fetch the test session tokens for '${userType}' user. Please check TEST_SESSIONS_URL and TEST_SESSIONS_API_KEY environment variables.`);
-	}
+const appendToken = (keys, userType, url, apiKey) => {
+	fetch(`${url}/${userType}?api_key=${apiKey}`)
+		.then((tokens) => {
+			if (tokens.FTSession) {
+				keys[`${userType.toUpperCase()}_FTSession`] = tokens.FTSession;
+			}
+			if (tokens.FTSession_s) {
+				keys[`${userType.toUpperCase()}_FTSession_s`] = tokens.FTSession_s;
+			}
+		})
+		.catch((e) => {
+			console.error(e);
+			console.log(`Couldn\'t fetch the test session tokens for '${userType}' user. Please check TEST_SESSIONS_URL and TEST_SESSIONS_API_KEY environment variables.`);
+		});
 };
