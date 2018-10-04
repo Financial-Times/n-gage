6990aef9f836c36bf92401e8b1f6a241188aed20
Matt Brennan <matt@153.io>
2018-10-04 15:36:20 +0100

    remove the help default target while i work out what the h*ck happened  🐿 v2.10.3

--- index.mk
+++ index.mk
@@ -48,9 +48,6 @@ MAKEFILE_HAS_A11Y = `grep -rli "a11y" Makefile`
 REPLACE_IN_GITIGNORE = sed -i -e 's/$1/$2/g' .gitignore && rm -f .gitignore-e ||:
 ENTRY_MAKEFILE = $(firstword $(MAKEFILE_LIST))
 
-# Show help when run without any target
-.DEFAULT_GOAL := help
-
 #
 # META TASKS
 #
