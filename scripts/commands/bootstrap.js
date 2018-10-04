a9d5203f77598c403eaf3784be973958369fd6d3
Matt Brennan <matt@153.io>
2018-10-04 14:44:18 +0100

    cool so that file moved didn't it  🐿 v2.10.3

--- scripts/commands/bootstrap.js
+++ scripts/commands/bootstrap.js
@@ -1,5 +1,5 @@
 exports.command = 'bootstrap';
 exports.describe = 'called by makefiles to include n-gage';
 exports.handler = () => {
-	console.log(require.resolve('../index.mk'));
+	console.log(require.resolve('../../index.mk'));
 };
