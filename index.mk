366415d9090f8d9f1ae78cc49f75879f48a96220
Simon Plenderleith <simon@simonplend.co.uk>
2018-10-04 14:01:56 +0100

    gage not makefile

--- index.mk
+++ index.mk
@@ -11,7 +11,7 @@ endif
 
 # Enforce repo ownership
 ifeq ("$(wildcard ft.yml)","")
-$(error 'Projects making use of n-makefile *must* define an ft.yml file containing the repo owner’s details (see any next- repo for required structure)')
+$(error 'Projects making use of n-gage *must* define an ft.yml file containing the repo owner’s details (see any next- repo for required structure)')
 $(error 'If you are creating a project not to be maintained by the next team please feel free to copy what you need from our build tools but don’t add an ft.yml.')
 $(error 'Integrating with our tooling may result in unwanted effects e.g. nightly builds, slack alerts, emails etc')
 endif
