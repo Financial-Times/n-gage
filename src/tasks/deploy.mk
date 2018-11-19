fbffaf6cf459f320f5855790e96930c433192a33
Matt Brennan <matt@153.io>
2018-11-19 11:29:15 +0000

    more whitespace  🐿 v2.10.3

--- src/tasks/deploy.mk
+++ src/tasks/deploy.mk
@@ -4,15 +4,21 @@ deplo%: ## deploy: deploy the app to heroku
 	$(call ASSERT_VARS_EXIST, HEROKU_APP_STAGING VAULT_NAME HEROKU_APP_EU HEROKU_APP_US)
 # Reset repository so that the app deploys on rebuilds even though there is no code change
 	heroku repo:reset -a ${HEROKU_APP_STAGING}
+
 	@echo "Deploying app to ${HEROKU_APP_STAGING}"
 	@git push https://git.heroku.com/$(HEROKU_APP_STAGING).git master
+
 	@echo "Setting ennvironment variables for ${HEROKU_APP_STAGING}..."
 	nht configure ${VAULT_NAME} ${HEROKU_APP_STAGING}
 	heroku dyno:scale web=1 -a $(HEROKU_APP_STAGING)
+
 	nht gtg ${HEROKU_APP_STAGING}
+
 	@n-test smoke -H https://${HEROKU_APP_STAGING}.herokuapp.com --header "FT-Next-Backend-Key: ${FT_NEXT_BACKEND_KEY}"
+
 	nht configure ${VAULT_NAME} ${HEROKU_APP_EU} --overrides REGION=EU
 	nht configure ${VAULT_NAME} ${HEROKU_APP_US} --overrides REGION=US
+
 	heroku pipelines:promote -a $(HEROKU_APP_STAGING)
 	heroku dyno:scale web=0 -a $(HEROKU_APP_STAGING)
 
