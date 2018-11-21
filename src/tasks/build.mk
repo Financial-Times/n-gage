c52c2262332fa95d9601034ff560ed9074bf2428
Arjun Gadhia <arjungadhia@gmail.com>
2018-11-21 11:15:14 +0000

    Remove haikro and use heroku slug commit for about.json  🐿 v2.7.0

--- src/tasks/build.mk
+++ src/tasks/build.mk
@@ -2,11 +2,10 @@ buil%: ## build: Build this repository.
 buil%: ## build-production: Build this repository for production.
 buil%: public/__about.json
 	@if [ -e webpack.config.js ]; then $(MAKE) $(subst build,assets,$@); fi
-	@if [ -e Procfile ] && [ "$(findstring build-production,$@)" == "build-production" ]; then haikro build; fi
 	@$(DONE)
 
 # BUILD SUB-TASKS
 
 # Only apply to Heroku apps for now
 public/__about.json:
-	@if [ -e Procfile ]; then mkdir -p public && echo '{"description":"$(call APP_NAME)","support":"next.team@ft.com","supportStatus":"active","appVersion":"$(shell git rev-parse HEAD | xargs echo -n)","buildCompletionTime":"$(shell date -u +"%Y-%m-%dT%H:%M:%SZ")"}' > $@ && $(DONE); fi
+	@if [ -e about.json ]; then mkdir -p public && echo '{"description":"$(call APP_NAME)","support":"next.team@ft.com","supportStatus":"active","appVersion":"$(HEROKU_SLUG_COMMIT)","buildCompletionTime":"$(shell date -u +"%Y-%m-%dT%H:%M:%SZ")"}' > $@ && $(DONE); fi
