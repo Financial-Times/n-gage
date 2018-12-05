89ae1628aec08b06a015c93a588b16e0318775cf
Tak Tran <contact@tutaktran.com>
2018-12-05 17:37:57 +0000

    Extract review-app as a separate file

--- src/tasks/deploy.mk
+++ src/tasks/deploy.mk
@@ -31,25 +31,6 @@ deplo%: ## deploy: deploy the app to heroku
 	heroku pipelines:promote -a $(HEROKU_APP_STAGING)
 	heroku dyno:scale web=0 -a $(HEROKU_APP_STAGING)
 
-tidy:
-	-rm .review-app
-
-review-app: tidy .review-app
-
-.review-app:
-	@echo 'Creating review app for $(VAULT_NAME)'
-	nht configure $(VAULT_NAME) review-app --overrides NODE_ENV=branch
-	@nht review-app $(VAULT_NAME) \
-		--repo-name $(CIRCLE_PROJECT_REPONAME) \
-		--branch $(CIRCLE_BRANCH) \
-		--commit $(CIRCLE_SHA1) \
-		--github-token $(GITHUB_AUTH_TOKEN) > $@
-
-gtg-review-app: review-app
-	nht gtg $(REVIEW_APP)
-
-test-review-app: gtg-review-app smoke a11y
-
 heroku-postbuil%:
 	npm update
 	@if [ -e bower.json ]; then $(BOWER_INSTALL); fi
