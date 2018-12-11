6f64982e8e23564d6426ba44b8674a187a94138c
Tak Tran <contact@tutaktran.com>
2018-12-11 12:43:07 +0000

    Revert "Reshuffle make tasks for deps 🐿 v2.11.0"
    This reverts commit 85bf12c8dc2aa5bfb0f830d704f8760f303baa0e.

--- src/tasks/review-app.mk
+++ src/tasks/review-app.mk
@@ -15,8 +15,7 @@ review-app: tidy-review-app .review-app
 gtg-review-app: review-app
 	nht gtg $(REVIEW_APP)
 
-run-test-review-app: gtg-review-app
-	$(MAKE) smoke a11y
-
 test-review-ap%:
-	$(MAKE) run-test-review-app
+	$(MAKE) gtg-review-app
+	TEST_URL="http://$$(cat .review-app).herokuapp.com" \
+		$(MAKE) smoke a11y
