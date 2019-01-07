ceab34022b798c41abf74ac16f16645fcecac200
bren <bren@153.io>
2019-01-07 14:23:42 +0000

    use SOURCE_VERSION not HEROKU_SLUG_COMMIT  🐿 v2.11.0

--- src/tasks/build.mk
+++ src/tasks/build.mk
@@ -8,4 +8,4 @@ buil%: public/__about.json
 
 # Only apply to Heroku apps for now
 public/__about.json:
-	@if [ -e app.json ]; then mkdir -p public && echo '{"description":"$(call APP_NAME)","support":"next.team@ft.com","supportStatus":"active","appVersion":"$(HEROKU_SLUG_COMMIT)","buildCompletionTime":"$(shell date -u +"%Y-%m-%dT%H:%M:%SZ")"}' > $@ && $(DONE); fi
+	@if [ -e app.json ]; then mkdir -p public && echo '{"description":"$(call APP_NAME)","support":"next.team@ft.com","supportStatus":"active","appVersion":"$(SOURCE_VERSION)","buildCompletionTime":"$(shell date -u +"%Y-%m-%dT%H:%M:%SZ")"}' > $@ && $(DONE); fi
