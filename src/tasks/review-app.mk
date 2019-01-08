2fbd2c767e7b3602e061939e193592d4b06e7783
Tak Tran <contact@tutaktran.com>
2019-01-08 17:06:38 +0000

    Allow override variable for overriding configure
    Add REVIEW_APP_CONFIGURE_OVERRIDES to override
    the override variable for nht configure   🐿 v2.11.0

--- src/tasks/review-app.mk
+++ src/tasks/review-app.mk
@@ -10,7 +10,9 @@ review-app: tidy-review-app .review-app
 
 .review-app:
 	@echo 'Creating review app for $(VAULT_NAME)'
-	nht configure $(VAULT_NAME) review-app --overrides NODE_ENV=branch
+	[ -z "$REVIEW_APP_CONFIGURE_OVERRIDES" ] \
+		&& nht configure $(VAULT_NAME) review-app --overrides NODE_ENV=branch || \
+		nht configure $(VAULT_NAME) review-app --overrides "$(REVIEW_APP_CONFIGURE_OVERRIDES)"
 	@nht review-app $(VAULT_NAME) \
 		--repo-name $(CIRCLE_PROJECT_REPONAME) \
 		--branch $(CIRCLE_BRANCH) \
@@ -20,7 +22,7 @@ review-app: tidy-review-app .review-app
 gtg-review-app: review-app
 	nht gtg $$(cat $(REVIEW_APP_FILE))
 
-test-review-ap%: # test-review-app: create and test a review app on heroku
+test-review-ap%: # test-review-app: create and test a review app on heroku. To override custom environment variables when running `nht configure`, add `REVIEW_APP_CONFIGURE_OVERRIDES="NODE_ENV=branch,OTHER_VAR=something" to the Makefile`
 	$(MAKE) gtg-review-app
 	TEST_URL="http://$$(cat $(REVIEW_APP_FILE)).herokuapp.com" \
 		$(MAKE) smoke a11y
