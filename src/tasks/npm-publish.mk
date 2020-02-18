0b294e7f3fd3087f2e5d56d046c8731fcfd2ae28
Matt Hinchliffe <matt@maketea.co.uk>
2020-02-18 09:19:56 +0000

    Remove npm publish task as this is provided by next-ci-shared-helpers

--- src/tasks/npm-publish.mk
+++
@@ -1,7 +0,0 @@
-npm-publis%: ## npm-publish: Publish this package to npm.
-ifneq ($(CIRCLE_TAG),)
-	npm version --no-git-tag-version $(CIRCLE_TAG)
-else
-	npm-prepublish --verbose
-endif
-	npm publish --access public
