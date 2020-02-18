9121a0e5eb2dc32c5a0be3eb2d2741e71c9a2785
Matt Hinchliffe <matt@maketea.co.uk>
2020-02-18 09:16:19 +0000

    Remove n-ui version parity check as no apps use n-ui now

--- src/tasks/verify.mk
+++ src/tasks/verify.mk
@@ -1,20 +1,8 @@
 verif%: ## verify: Verify this repository.
-verif%: ci-n-ui-check _verify_lintspaces _verify_eslint _verify_stylelint
+verif%: _verify_lintspaces _verify_eslint _verify_stylelint
 	@$(DONE)
 
 # DEPLOY SUB-TASKS
-
-ci-n-ui-check:
-# In CircleCI
-ifneq ($(CIRCLE_BUILD_NUM),)
-# The app is using n-ui
-ifneq ($(shell grep -s -Fim 1 '"n-ui"' bower.json),)
-# versions in package.json and bower.json are not equal
-ifneq ($(shell awk '$$1 == "\"version\":" {print $$2}' bower_components/n-ui/.bower.json | sed s/,//),$(shell awk '$$1 == "\"version\":" {print $$2}'  node_modules/@financial-times/n-ui/package.json | sed s/,//))
-	$(error 'Projects using n-ui must maintain parity between versions. Rebuild without cache and update your bower.json and package.json if necessary. If this error persists make sure that the n-ui build succeeded in publishing a new version to NPM (https://circleci.com/gh/Financial-Times/n-ui) and that both NPM and Bower registries have the latest version.')
-endif
-endif
-endif
 
 _verify_eslint:
 	@if [ -e .eslintrc.js ]; then $(call GLOB,'*.js') | xargs eslint --ignore-pattern '!' --fix && $(DONE); fi
