bf9085c581a70ee03b0f3a5c5694f2f769804b01
Anton Samper Rivaya <antonsamper@users.noreply.github.com>
2020-09-23 10:32:50 +0100

    chore(review-app): display logs

--- src/tasks/review-app.mk
+++ src/tasks/review-app.mk
@@ -17,7 +17,7 @@ review-app: tidy-review-app .review-app
 	  nht configure $(VAULT_NAME) review-app --overrides NODE_ENV=branch \
 	)
 
-	@nht review-app $(VAULT_NAME) \
+	nht review-app $(VAULT_NAME) \
 		--repo-name $(CIRCLE_PROJECT_REPONAME) \
 		--branch $(CIRCLE_BRANCH) \
 		--commit $(CIRCLE_SHA1) \
