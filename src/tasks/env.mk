5c3d566e32e6ffa037a44c603f162af5eafb71ee
Estefania Morton <estefania.morton@ft.com>
2020-10-07 13:07:42 -0500

    missing brackets

--- src/tasks/env.mk
+++ src/tasks/env.mk
@@ -2,8 +2,8 @@
 # Environment variables come from https://github.com/Financial-Times/next-vault-sync
 .env: ## .env: Downloads environment variables from Vault
 	@if [[ -z "$(shell command -v vault)" ]]; then echo "Error: You don't have Vault installed. Follow the guide at https://github.com/Financial-Times/vault/wiki/Getting-Started-With-Vault"; exit 1; fi
-	@if [  -z "$(shell find ~/.vault-token -mmin -480)" ]; then \
-		if [ -z "$(VAULT_AUTH_GITHUB_TOKEN)" ]; then \
+	@if [[  -z "$(shell find ~/.vault-token -mmin -480)" ]]; then \
+		if [[ -z "$(VAULT_AUTH_GITHUB_TOKEN)" ]]; then \
 			echo "Error: VAULT_AUTH_GITHUB_TOKEN variable is not set. Follow the guide at https://github.com/Financial-Times/vault/wiki/Getting-Started-With-Vault"; exit 1; \
 		else \
 			echo "You are not logged in, logging you in..." && vault login --method github token=$$VAULT_AUTH_GITHUB_TOKEN; \
